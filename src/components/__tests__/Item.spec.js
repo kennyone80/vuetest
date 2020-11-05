1b5439b662040368c395ee150a6e08c60250f6bf
Eric <eric.bernier@outlook.com>
2020-11-05 23:14:58 +0100

    First testing component

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -2,7 +2,12 @@ import Item from '../Item.vue'
 import Vue from 'vue'
 
 describe('Item.vue', () => {
-  test('sanity test', () => {
-    console.log(Item)
+  test('renders "item"', () => {
+    // Creates a new Vue constructor with the Item options
+    const Ctor = Vue.extend(Item)
+    // Creates a new Vue instance, and mounts the Vue instance
+    const vm = new Ctor().$mount()
+    // Acces the DOM element, and check the text content
+    expect(vm.$el.textContent).toContain('item')
   })
 })
