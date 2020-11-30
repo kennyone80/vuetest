9c2e088532f78a3f92d887ccbb07361adc1c8994
kennyone80 <43655328+kennyone80@users.noreply.github.com>
2020-11-30 22:36:11 +0000

    Test ProgressBar

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,8 +1,30 @@
 <template>
     <div
-        class="hidden"
+        :class="{
+            hidden: hidden
+        }"
         :style="{
-            'width': '0%'
+            'width': `${percent}%`
             }">
     </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      hidden: true,
+      percent: 0
+    }
+  },
+  methods: {
+    start () {
+      this.hidden = false
+      this.percent = 50
+    },
+    finish () {
+      this.hidden = true
+      this.percent = 100
+    }
+  }
+}
+</script>
--- src/components/__tests__/ProgressBar.spec.js
+++ src/components/__tests__/ProgressBar.spec.js
@@ -11,4 +11,26 @@ describe('ProgressBar.vue', () => {
     const wrapper = shallowMount(ProgressBar)
     expect(wrapper.element.style.width).toBe('0%')
   })
+
+  // test('displays the bar when start is called', () => {
+  //   const wrapper = shallowMount(ProgressBar)
+  //   expect(wrapper.classes()).toContain('hidden')
+  //   wrapper.vm.start()
+  //   expect(wrapper.classes()).not.toContain(['hidden'])
+  // })
+
+  test('sets the bar to 100% width when finish is called', () => {
+    const wrapper = shallowMount(ProgressBar)
+    wrapper.vm.start()
+    wrapper.vm.finish()
+    expect(wrapper.element.style.width).toBe('100%')
+  })
+
+  test('hide the bar when finish is called', () => {
+    const wrapper = shallowMount(ProgressBar)
+    expect(wrapper.classes()).toContain('hidden')
+    wrapper.vm.start()
+    wrapper.vm.finish()
+    expect(wrapper.classes()).not.toContain(['hidden'])
+  })
 })
